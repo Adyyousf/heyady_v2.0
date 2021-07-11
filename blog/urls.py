c0c79791d571e556d8237a2e16d9e92b2e4a51dd
AdyYousf <adyyousf197@gmail.com>
2021-07-12 01:41:40 +0530

    heyady version 2.0 is up and live

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # API to post comment
+    path('postComment', views.postComment, name='postComment'),
+
+    
+    path('', views.blogHome, name='blogHome'),
+    path('<str:slug>', views.blogPost, name='blogPost'),
+
+]
+ 
---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from blog.models import Post, BlogComment
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
+# Create your views here.
+
+
+def blogHome(request):
+    allPosts = Post.objects.all()
+    context = {'allPosts': allPosts}
+    return render(request, 'blog/blogHome.html', context)
+
+
+def blogPost(request, slug):
+    post = Post.objects.filter(slug=slug).first()
+    comments = BlogComment.objects.filter(post=post)
+    context = {'post': post, 'comments': comments}
+    return render(request, 'blog/blogPost.html', context)
+
+
+def postComment(request):
+    if request.method == "POST":
+        comment = request.POST.get("comment")
+        user = request.user
+        postSno = request.POST.get("postSno")
+        post = Post.objects.get(sno=postSno)
+
+        comment = BlogComment(comment=comment, user=user, post=post)
+        comment.save()
+        messages.success(request, "your comment has been posted succesfully")
+
+    return redirect(f"/blog/{post.slug}")
