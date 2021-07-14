4eb5f24419256ab874f9d63a75534ab2b9eb7211
AdyYousf <adyyousf197@gmail.com>
2021-07-14 23:23:09 +0530

    add RTE and view counter

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Post, BlogComment
+from . models import Post, BlogComment
 
 # Register your models here.
-admin.site.register((Post, BlogComment))
+admin.site.register((BlogComment,))
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('tinyInject.js',)
